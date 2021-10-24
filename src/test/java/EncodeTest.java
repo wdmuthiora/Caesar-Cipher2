5ad831daea10f85acaff651539ff6d4bb7e1601f
wdmuthiora <wdmuthiora@gmail.com>
2021-10-24 17:56:22 +0300

    Created Encode and Decode classes, and tested their instantiation.

---
+++ src/test/java/EncodeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncodeTest {
+    @Test
+    public void newEncode_instantiatesCorrectly(){
+        Encode encode = new Encode();
+    }
+}
