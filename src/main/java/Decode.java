193cb27995a0ae08b1d4d741fb5fdf65ae2b3a26
wdmuthiora <wdmuthiora@gmail.com>
2021-10-25 17:57:32 +0300

    Created the decoding class's main method.

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Decode {
+    public static void main(String[] args) {
 
+//        Use Scanner instead for user interface.
+        Scanner scan = new Scanner(System.in);
+
+//        Ask user for encryptedText.
+        System.out.println("Enter the text message to be decrypted: ");
+        String stringToBeDecrypted = new String();
+        stringToBeDecrypted = scan.next();
+
+//        Ask user for decryption key
+        System.out.println("Enter a number between 0 and 26: ");
+        String decryptionKey = new String();
+        decryptionKey = scan.next();
+        Integer key =Integer.parseInt(decryptionKey);
+        System.out.println(" Decrypted Text : " + decoding(stringToBeDecrypted, key));
+        scan.close();
+    }
 }
--- src/main/java/Encode.java
+++ src/main/java/Encode.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 class Encode {
     public static void main(String[] args) {
-        //Use Scanner instead for user interface.
+
+//        Use Scanner instead for user interface.
         Scanner scan = new Scanner(System.in);
+
 //        Ask user for plainText.
         System.out.println("Enter the text message to be encrypted: ");
         String stringToBeEncrypted = new String();
         stringToBeEncrypted = scan.next();
+
 //        Ask user for key
         System.out.println("Enter a number between 0 and 26: ");
         String encryptionKey = new String();
@@ -17,18 +20,24 @@ class Encode {
     }
 
     private static String encoding(String stringToBeEncrypted, int encryptionKey) {
-    // String to be returned, in encrypted form.
+
+//        String to be returned, in encrypted form.
         String encryptedString ="";
-    //  introduce loop to iterate for each stringToBeEncrypted character.
+
+//        Introduce loop to iterate for each stringToBeEncrypted character.
         for (int i=0; i<stringToBeEncrypted.length(); i++){
             char singleCharacter = stringToBeEncrypted.charAt(i);
-            //If singleCharacter is a letter
+
+//            If singleCharacter is a letter
             if (Character.isLetter(singleCharacter)){
-                //Check if singleCharacter is uppercase/lowercase using ASCII values
+
+//               Check if singleCharacter is uppercase/lowercase using ASCII values
                 if (Character.isLowerCase(singleCharacter)){
-                    //Create a new encoded character, using casting.
+
+//                   Create a new encoded character, using casting.
                     char encodedSingleCharacter = (char)(singleCharacter+encryptionKey);
-                    //Make sure the encoded character falls within the lowercase alphabet bounds of ASCII.
+
+//                   Make sure the encoded character falls within the lowercase alphabet bounds of ASCII.
                     if (encodedSingleCharacter>'z'){
                         encryptedString += (char)(singleCharacter-(26-encryptionKey));
                     }else{
@@ -36,9 +45,11 @@ class Encode {
                     }
                 }
                 else if (Character.isUpperCase(singleCharacter)){
-                    //Create a new encoded character, using casting.
+
+//                   Create a new encoded character, using casting.
                     char encodedSingleCharacter = (char)(singleCharacter+encryptionKey);
-                    //Make sure the encoded character falls within the lowercase alphabet bounds of ASCII.
+
+//                   Make sure the encoded character falls within the lowercase alphabet bounds of ASCII.
                     if (encodedSingleCharacter>'Z'){
                         encryptedString += (char)(singleCharacter-(26-encryptionKey));
                     }else{
@@ -46,13 +57,12 @@ class Encode {
                     }
                 }
             }
-            //If singleCharacter is not a letter, push that character into encryptedString.
+
+//           If singleCharacter is not a letter, push that character into encryptedString.
             else {
-                encryptedString+=singleCharacter;
+                encryptedString += singleCharacter;
             }
         }
-
-
         return encryptedString;
     }
 }
